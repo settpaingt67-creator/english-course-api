6e678d6cb9695f6b3b4e37c0257e7e718bd22c3f
Sett Paing Tun <settpaing.t67@rsu.ac.th>
2026-04-25 12:25:45 +0700

    vercel added

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// ROUTES
 const courseRoutes = require("./routes/courses");
-app.use("/api/courses", courseRoutes);
-
 const authRoutes = require("./routes/auth");
-app.use("/api/auth", authRoutes);
-
 const userRoutes = require("./routes/users");
+
+app.use("/api/courses", courseRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
+// ROOT
 app.get("/", (req, res) => {
-  res.send("API running");
+  res.send("API running...");
 });
 
 module.exports = app;
